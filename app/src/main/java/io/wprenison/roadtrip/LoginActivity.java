4b67a1c4a9f4746d930e465f100494233ef93286
wprenison <wprenison@gmail.com>
2016-09-14 04:14:16 +0200

    Initial commit

---
+++ app/src/main/java/io/wprenison/roadtrip/LoginActivity.java
@@ -0,0 +1,20 @@
+package io.wprenison.roadtrip;
+
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * A login screen that offers login via google sign in
+ */
+public class LoginActivity extends AppCompatActivity
+{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+    {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
+
